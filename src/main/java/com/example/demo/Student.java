f37ab2ff84983bb003a9b12b13f9b860f767afe0
jackhsu <jack23h67@hotmail.com>
2022-09-23 16:12:40 +0800

    2022/09/23 1612

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import java.util.List;
+
+public class Student {
+	Integer id;
+	String name;
+	Double score;
+	boolean graduated;
+	Integer age;
+	List<String> courseList;
+	Pet pet;
+	List<Pet> petList;
+}
