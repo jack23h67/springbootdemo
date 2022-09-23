a9675fb69aeab88c99a24b0e6b585e53fef59723
jackhsu <jack23h67@hotmail.com>
2022-09-23 17:28:23 +0800

    2022/09/23 1728

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -11,21 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
-	@RequestMapping("/product")
-	public Store product() {
-		Store store = new Store();
-		List<String> pList = new ArrayList<String>();
-		pList.add("蘋果");
-		pList.add("橘子");
-		store.setProductList(pList);
-		return store;	
+	@RequestMapping("/test1")
+	public String test1(@RequestParam Integer id,
+			            @RequestParam(required=false) String  name,
+			            @RequestParam(defaultValue="10") Integer age) {
+		System.out.println("id:" + id);
+		System.out.println("name:" + name);
+		System.out.println("age:" + age);
+		return "Hello test1";	
 	}
-	
-	@RequestMapping("/user")
-	public Student user() {
-		Student student = new Student();
-		student.setName("Judy");
-		student.setId(1);
-		return student;
-	}
+
 }
