768f66cfb499a88f91a066ad005e9539bb827601
jackhsu <jack23h67@hotmail.com>
2022-09-23 14:22:32 +0800

    2022/09/23 1422

--- src/main/java/com/example/demo/MyAspect.java
+++ src/main/java/com/example/demo/MyAspect.java
@@ -1,21 +1,19 @@
 package com.example.demo;
 
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyAspect {
 	
-	@Before("execution(* com.example.demo.HpPrinter.*(..))")
-	public void before() {
-		System.out.println("I'm before");
-	}
-	
-	@After("execution(* com.example.demo.HpPrinter.*(..))")
-	public void after() {
-		System.out.println("I'm after");
+	@Around("execution (* com.example.demo.HpPrinter.*(..))")
+	public Object around(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("I'm around Before");
+		Object obj = pjp.proceed();
+		System.out.println("I'm around After");
+		return obj;
 	}
 }
