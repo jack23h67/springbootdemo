cf1647087c91b161f397762bcb6af1a8685d4756
jackhsu <jack23h67@hotmail.com>
2022-09-23 17:43:39 +0800

    2022/09/23 1743

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +19,11 @@ public class MyController {
 		System.out.println("age:" + age);
 		return "Hello test1";	
 	}
-
+	
+	@RequestMapping("/test2")
+	public String test2(@RequestBody Student student) {
+		System.out.println("student id:"   + student.getId());
+		System.out.println("student name:" + student.getName());
+		return "Hello test2";
+	}
 }
