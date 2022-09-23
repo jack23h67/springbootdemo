af90df23536604d31c526197306343786753d748
jackhsu <jack23h67@hotmail.com>
2022-09-23 14:07:08 +0800

    2022/09/23 1407

--- src/main/java/com/example/demo/MyAspect.java
+++ src/main/java/com/example/demo/MyAspect.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -11,5 +12,10 @@ public class MyAspect {
 	@Before("execution(* com.example.demo.HpPrinter.*(..))")
 	public void before() {
 		System.out.println("I'm before");
+	}
+	
+	@After("execution(* com.example.demo.HpPrinter.*(..))")
+	public void after() {
+		System.out.println("I'm after");
 	}
 }
