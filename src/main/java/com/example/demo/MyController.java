9ee5f775be68d91afe0bd05aee8c2ce1e2f6fa10
jackhsu <jack23h67@hotmail.com>
2022-09-26 13:59:04 +0800

    2022/09/26 1400

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
+	@RequestMapping("/test")
+	public ResponseEntity<String> test() {
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body("Hello World");	
+	}
+	
 	@RequestMapping("/test1")
 	public String test1(@RequestParam Integer id,
 			            @RequestParam(required=false) String  name,
