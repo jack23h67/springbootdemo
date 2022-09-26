ff8b6cde36c403be0a99093206b8123283767420
jackhsu <jack23h67@hotmail.com>
2022-09-26 19:51:16 +0800

    2022/09/26 1950

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -5,7 +5,10 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,14 +18,26 @@ public class StudentController {
 	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	
 	@PostMapping("/students")
-	public String insert() {
-		String sql = "INSERT INTO student(id,name) VALUES(3,'John')";
+	public String insert(@RequestBody Student student) {
+		String sql = "INSERT INTO student(id,name) VALUES(:studentId, :studentName)";
 		
 		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("studentId", student.getId());
+		map.put("studentName", student.getName());
 		
 		namedParameterJdbcTemplate.update(sql, map);
 		
 		return "執行INSERT sql";
 	}
 
+	@DeleteMapping("/students/{studentId}")
+	public String delete(@PathVariable Integer studentId) {
+		String sql = "DELETE FROM student WHERE id = :studentId";
+		
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("studentId", studentId);
+		namedParameterJdbcTemplate.update(sql, map);
+		
+		return "執行DELETE sql";
+	}
 }
