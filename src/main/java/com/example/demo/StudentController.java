a311ac47683d1cacd9f62d2ab90cbb9b30275cf7
jackhsu <jack23h67@hotmail.com>
2022-09-27 10:39:00 +0800

    2022/09/27 1040 update完 select all完

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -1,11 +1,16 @@
 package com.example.demo;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,15 +24,47 @@ public class StudentController {
 	
 	@PostMapping("/students")
 	public String insert(@RequestBody Student student) {
-		String sql = "INSERT INTO student(id,name) VALUES(:studentId, :studentName)";
+		String sql = "INSERT INTO student(name) VALUES(:studentName)";
 		
 		Map<String,Object> map = new HashMap<String,Object>();
-		map.put("studentId", student.getId());
 		map.put("studentName", student.getName());
 		
-		namedParameterJdbcTemplate.update(sql, map);
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+
+		namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(map),keyHolder);
 		
-		return "執行INSERT sql";
+		int id = keyHolder.getKey().intValue();
+		System.out.println("mySql自動生成的id為："+id);
+		
+		return "執行 INSERT sql";
+	}
+	
+	@PostMapping("/students/batch")
+	public String insertList(@RequestBody List<Student> studentList) {
+		String sql = "INSERT INTO student (name) VALUES (:studentName)";
+		
+		MapSqlParameterSource[] parmentSource = new MapSqlParameterSource[studentList.size()];
+		
+		for (int i = 0; i < studentList.size();i++) {
+			Student student = studentList.get(i);
+			
+			parmentSource[i] = new MapSqlParameterSource();
+			parmentSource[i].addValue("studentName", student.getName());
+		}
+		
+		namedParameterJdbcTemplate.batchUpdate(sql,parmentSource);
+		return "執行一批 INSERT sql";
+	}
+	
+	@GetMapping("/students")
+	public List<Student> select() {
+		String sql = "SELECT id, name FROM student";
+		
+		Map<String,Object> map = new HashMap<String,Object>();
+		
+		List<Student> slist = namedParameterJdbcTemplate.query(sql,map, new StudentRowMapper());
+		
+		return slist;
 	}
 
 	@DeleteMapping("/students/{studentId}")
