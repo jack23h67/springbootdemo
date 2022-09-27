ec4b5d1d1ce2cc0c5c5c9c668e4639cbaf74322f
jackhsu <jack23h67@hotmail.com>
2022-09-27 17:34:07 +0800

    2022/09/27 1735 6-6

--- src/main/java/com/example/demo/StudentRepository.java
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepository extends CrudRepository<Student, Integer> {
-
+	@Query(value = "SELECT id,name FROM student WHERE id = ?1 AND name = ?2", nativeQuery = true)
+			Student test1(Integer id, String name);
+	
+	List<Student> findByName(String name);
+	Student findByIdAndName(Integer id, String name);
 }
