e54fa02957b1f511e260d408b1dc53e5f4e11c65
jackhsu <jack23h67@hotmail.com>
2022-09-28 20:12:27 +0800

    2022/09/28 2012 7-8 controller test

--- src/test/java/com/example/demo/controller/StudentControllerTest.java
+++ src/test/java/com/example/demo/controller/StudentControllerTest.java
@@ -1,14 +1,21 @@
 package com.example.demo.controller;
 
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,11 +26,31 @@ public class StudentControllerTest {
 	
 	@Test
 	public void testGetById() throws Exception {
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/students/3");
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get("/students/{studentId}", 3)
+				.header("headerName", "headerValue")
+				.queryParam("graduate", "true");
 		
-		mockMvc.perform((org.springframework.test.web.servlet.RequestBuilder) requestBuilder)
-				.andExpect(MockMvcResultMatchers.status().is(200));
+		MvcResult mvcResult = mockMvc.perform((org.springframework.test.web.servlet.RequestBuilder) requestBuilder)
+				.andDo(print())
+		        .andExpect(status().is(200))
+				.andExpect(jsonPath("$.id", equalTo(3)))
+				.andExpect(jsonPath("$.name", notNullValue()))
+				.andReturn();
 		
+		String body = mvcResult.getResponse().getContentAsString();
+		
+		System.out.println(body);
 	}
 
+	@Test
+	public void testCreate() throws Exception{
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.post("/students")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"name\":\"Hank\",\"score\":14.6,\"graduate\":false}");
+		
+		mockMvc.perform(requestBuilder)
+		       .andExpect(status().is(201)); 
+	}
 }
