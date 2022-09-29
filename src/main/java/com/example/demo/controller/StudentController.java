f577d07a5d276bac09e95bb62af5d31b53e10788
jackhsu <jack23h67@hotmail.com>
2022-09-29 18:03:23 +0800

    2022/09/29 1802 8-4 log

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,14 +1,25 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.Student;
-import com.example.demo.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Student;
+import com.example.demo.service.StudentService;
 
 @RestController
 public class StudentController {
+	
+	private final static Logger log = LoggerFactory.getLogger(StudentController.class);
 
     @Autowired
     private StudentService studentService;
@@ -43,6 +54,10 @@ public class StudentController {
 
     @GetMapping("/students/{studentId}")
     public ResponseEntity<Student> read(@PathVariable Integer studentId) {
+    	
+    	log.info("get student {}",studentId);
+    	//log.warn("");
+    	//log.error("");
 
         Student student = studentService.getById(studentId);
 
--- src/test/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS student (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30),
-    score DOUBLE,
-    graduate BOOLEAN,
-    create_date TIMESTAMP
-);
